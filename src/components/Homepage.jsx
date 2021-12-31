b4321d28936c3bea1d8f9c5329e16c03a8cb3be1
Mriganka Saha <mrigankasaha2@gmail.com>
2021-12-31 18:09:41 +0530

    Home page design

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,36 @@
 import React from "react";
 
 export default function Homepage() {
-  return <div>This is homepage</div>;
+  return (
+    <div>
+      <div className="text-center pb-12 md:pb-16">
+        <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4 text-gray-900 dark:text-gray-50">
+          Hi, My Name is{" "}
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+            Mriganka
+          </span>
+        </h1>
+        <div className="max-w-3xl mx-auto">
+          <p className="text-xl text-gray-600 dark:text-stone-300 mb-8">
+            I am a software developer with more than 4 years of experience and
+            currently working for a Big 4 accountancy farm.
+          </p>
+          <div className="max-w-xs mx-auto sm:max-w-none flex flex-col sm:flex-row justify-center items-center">
+            <a
+              className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-48 m-4"
+              href="/skills"
+            >
+              View My Skills
+            </a>
+            <a
+              className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-48 m-4"
+              href="/about"
+            >
+              Learn More
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
