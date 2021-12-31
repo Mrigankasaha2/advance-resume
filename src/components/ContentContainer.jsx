c83b59075618a25c3e91f100932f57252fdac8f6
Mriganka Saha <mrigankasaha2@gmail.com>
2021-12-31 09:17:57 +0530

    Design of all component

---
+++ src/components/ContentContainer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TopNavigation from "./TopNavigation";
+
+export default function ContentContainer() {
+  return (
+    <div className="content-container">
+      <TopNavigation />
+    </div>
+  );
+}
