f9046d347722b4057bdae62b87d029d3f4e9b500
Mriganka Saha <mrigankasaha2@gmail.com>
2021-12-31 10:11:11 +0530

    Add all component and react-router-dom

---
+++ src/components/Aboutme.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Aboutme() {
+  return <div>This is about me</div>;
+}
--- src/components/ContentContainer.jsx
+++ src/components/ContentContainer.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import TopNavigation from "./TopNavigation";
+import Homepage from "./Homepage";
+import Skills from "./Skills";
+import Education from "./Education";
+import Experience from "./Experience";
+import Aboutme from "./Aboutme";
 
 export default function ContentContainer() {
   return (
@@ -7,6 +13,15 @@ export default function ContentContainer() {
       <TopNavigation />
       <div className="content-list">
         <span>This is the main content</span>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/about" element={<Aboutme />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </div>
   );
