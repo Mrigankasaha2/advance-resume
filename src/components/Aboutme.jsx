49d7fa13a86a4d45491ee7d517560d9fa6313a81
Mriganka Saha <mrigankasaha2@gmail.com>
2023-05-11 11:32:06 +0530

    Minor Changes in Item

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -16,15 +16,26 @@ export default function Aboutme() {
       <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
         <blockquote>
           <p className="text-lg font-medium dark:text-gray-300">
-            “I am a full stack developer with work experience in .NET Core,
-            Angular, SQL Server, and Azure”
+            “I am a driven and results-focused professional with over 5.5 years
+            of experience in the technology industry. My expertise lies in
+            developing and maintaining applications using the Microsoft
+            technology stack, including .NET Core, Angular, and SQL Server. I am
+            also a Microsoft Certified Azure Developer and have worked with
+            various Azure PaaS solutions, including Web App, Function App, Azure
+            Kubernetes Services, Web Jobs, Service Bus, Event Hub, and others.
+            In addition to my technical skills, I have also acquired knowledge
+            of other technologies such as React, MongoDB, Docker, SignalR,
+            TailwindCSS, SCSS, Material UI, and others. I have a basic
+            understanding of creating a CICD pipeline using Azure DevOps and
+            GitHub Action. My goal is to continue growing my technical knowledge
+            and bring value to my clients by delivering high-quality solutions.”
           </p>
         </blockquote>
         <figcaption className="font-medium">
           <div className="text-indigo-500 dark:text-indigo-400">
             Mriganka Shekhar Saha
           </div>
-          <div className="text-gray-700 dark:text-gray-500">
+          <div className="text-gray-700 dark:text-gray-300">
             - Senior Software Engineer, EY
           </div>
         </figcaption>
@@ -39,7 +50,7 @@ export default function Aboutme() {
       <div className="pt-4 pb-2">
         <Socialmediaicons
           icons={<SiLinkedin size={28} className="text-blue-700" />}
-          link="https://www.linkedin.com/in/mriganka-saha-ba16ba13b/"
+          link="https://www.linkedin.com/in/mriganka-shekhar-saha-ba16ba13b/"
         />
         <Socialmediaicons
           icons={<SiGithub size={28} className="text-purple-700" />}
@@ -51,7 +62,7 @@ export default function Aboutme() {
         />
         <Socialmediaicons
           icons={<SiFacebook size={28} className="text-blue-600" />}
-          link="https://www.facebook.com/mriganka.saha.1996"
+          link="https://www.facebook.com/mriganka.saha.28"
         />
       </div>
     </figure>
